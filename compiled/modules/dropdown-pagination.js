53b5db6b73030d03125ab699da289998d6d0986c
Elijah Ervin <elijah.ervin@oneil.com>
2020-05-01 09:08:56 -0400

    Initial commit for webmaster

---
+++ compiled/modules/dropdown-pagination.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var debounce = require('debounce');
+
+module.exports = function (h) {
+    var _this = this;
+
+    return function (selectClass, id) {
+        var pages = [];
+        var selected;
+
+        for (var pag = 1; pag <= _this.totalPages; pag++) {
+            var selected = _this.page == pag;
+            pages.push(h('option', {
+                attrs: { value: pag },
+                domProps: {
+                    selected: selected
+                }
+            }, [pag]));
+        }
+        return h('select', {
+            class: selectClass + ' dropdown-pagination',
+            directives: [{
+                name: 'show',
+                value: _this.totalPages > 1
+            }],
+            attrs: {
+                name: 'page',
+                value: _this.page,
+                id: id
+            },
+            ref: 'page',
+            on: {
+                change: _this.setPage.bind(_this, null, false)
+            }
+        }, [pages]);
+    };
+};
---
+++ compiled/modules/per-page.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function (h) {
+    var _this = this;
+
+    return function (perpageValues, cls, id) {
+        return perpageValues.length > 1 ? h('select', {
+            class: cls,
+            attrs: {
+                name: 'limit',
+                value: _this.limit,
+                id: id
+            },
+            on: {
+                change: _this.setLimit.bind(_this)
+            }
+        }, [perpageValues]) : '';
+    };
+};
